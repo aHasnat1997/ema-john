635186c31bb8b7394240364a3e7a2baa61edc065
aHasnat1997 <hasnat56703@gmail.com>
2023-03-27 23:19:39 +0600

    card calculation add

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import logo from '../assets/images/Logo.svg';
 
-const Nav = props => {
-  // console.log(props.card);
-  const items = props.card;
+const Nav = ({card}) => {
+  // console.log(card);
+
+  let totalPrice = 0;
+  let shipping = 0;
+  for(const price of card){
+    totalPrice += price.price;
+    shipping += price.shipping;
+  }
+  const tex = (totalPrice*7)/100;
+  const total = totalPrice+shipping+tex;
+
   return (
     <div className='bg-primary w-full fixed top-0 z-50'>
       <div className="navbar max-w-screen-xl mx-auto">        
@@ -17,13 +26,13 @@ const Nav = props => {
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <div className="indicator text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                <span className="badge badge-sm indicator-item">{items.length}</span>
+                <span className="badge badge-sm indicator-item">{card.length}</span>
               </div>
             </label>
             <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
               <div className="card-body">
-                <span className="font-bold text-lg">{items.length} Items Add</span>
-                <span className="text-info">Subtotal: $999</span>
+                <span className="font-bold text-lg">{card.length} Items Add</span>
+                <span className="text-primary">Total: ${parseFloat(total.toFixed(2))}</span>
                 <div className="card-actions">
                   <button className="btn btn-primary btn-block">View cart</button>
                 </div>
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 
-const Order = props => {
+const Order = ({card}) => {
   // console.log(props.card);
-  const card = props.card;
+
+  let totalPrice = 0;
+  let shipping = 0;
+  for(const price of card){
+    totalPrice += price.price;
+    shipping += price.shipping;
+  }
+  const tex = (totalPrice*7)/100;
+  const total = totalPrice+shipping+tex;
+  // console.log(totalPrice);
+
   return (
     <div>
       <h1 className='text-center lg:text-3xl font-bold hidden lg:block'>Order Summary</h1>
       <div className="mt-[-1.5rem] md:mt-[-.5rem] lg:mt-8 text-xs md:text-sm lg:text-xl  grid grid-cols-2 md:grid-cols-5 lg:grid-cols-1 lg:gap-4">
         <p>Selected Items: {card.length}</p>
-        <p>Total Price: $0</p>
-        <p>Shipping Charge: $5</p>
-        <p>Tax: $0</p>
-        <p className='font-bold'>Grand Total: $0</p>
+        <p>Total Price: ${totalPrice}</p>
+        <p>Shipping Charge: ${shipping}</p>
+        <p>Tax: ${parseFloat(tex.toFixed(2))}</p>
+        <p className='font-bold'>Grand Total: ${parseFloat(total.toFixed(2))}</p>
       </div>
     </div>
   );
